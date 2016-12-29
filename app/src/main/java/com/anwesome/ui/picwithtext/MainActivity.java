8c6750e36688a5a2dbe9b0063ba2529c86805753
Anwesh43 <anweshthecool0@gmail.com>
2016-12-29 21:10:37 +0530

    Adding picwithtextmodule demo

---
+++ app/src/main/java/com/anwesome/ui/picwithtext/MainActivity.java
@@ -0,0 +1,14 @@
+package com.anwesome.ui.picwithtext;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.anwesome.ui.picwithtextmodule.CameraCaptureActivity;
+
+public class MainActivity extends CameraCaptureActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
